df5db99e65fbb327447fccd661777d29d5d695da
Ziyi.Dong <dongziyics@163.com>
2025-05-15 12:40:34 +0800

    add code of CPA

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-
 #include "pbc.h"
+
+// 定义时间服务器参数结构体
+typedef struct ts_params
+{
+    element_t g, g1, h, e_g_g, e_g_h;
+} ts_params;
+
+// 定义时间服务器参数结构体
+typedef struct pkg_params
+{
+    element_t g, g1, h, e_g_g, e_g_h;
+} pkg_params;
 
 int main()
 {
-    pairing_t pairing;   // 定义配对对象
-    element_t generator; // 定义生成元元素
+    pairing_t pairing;           // 定义配对对象
+    element_t ts_priv, pkg_priv; // 定义私钥对象
 
     // 1. 初始化配对参数（使用预定义的 Type A 参数）
     const char *param_str =
@@ -19,20 +30,68 @@ int main()
         "sign1 1\n"
         "sign0 1\n";
 
-    pairing_init_set_buf(pairing, param_str, strlen(param_str)); // 从字符串加载参数[6]
+    pairing_init_set_buf(pairing, param_str, strlen(param_str)); // 从字符串加载参数
 
-    // 2. 生成椭圆曲线生成元（G1群的基点）
-    element_init_G1(generator, pairing); // 初始化G1群元素
+    pkg_params pkg_params; // 定义时间服务器参数结构体
+    ts_params ts_params;   // 定义时间服务器参数结构体
 
-    element_random(generator); // 随机生成G1群的生成元[3,11]
+    element_init_Zr(ts_priv, pairing); // Zr 是一个整数模 r 的环，r 是群的阶
+    element_random(ts_priv);           // ts_priv ← 随机值 ∈ Zr
 
-    // 3. 打印生成元坐标
-    printf("生成元坐标:\n");
-    element_printf("X = %B\n", generator); // 输出生成元的x,y坐标[6,11]
+    element_init_Zr(pkg_priv, pairing); // Zr 是一个整数模 r 的环，r 是群的阶
+    element_random(pkg_priv);           // pks_priv ← 随机值 ∈ Zr
 
-    // 4. 清理内存
-    element_clear(generator);
+    // 生成椭圆曲线生成元（G1群的基点）
+    element_init_G1(ts_params.g, pairing); // 初始化G1群中TS参数
+    element_init_G1(ts_params.h, pairing);
+    element_init_G1(ts_params.g1, pairing);
+    element_init_GT(ts_params.e_g_g, pairing);
+    element_init_GT(ts_params.e_g_h, pairing);
+    element_random(ts_params.g);
+    element_random(ts_params.h);
+    element_mul_zn(ts_params.g1, ts_params.g, ts_priv);
 
+    pairing_apply(ts_params.e_g_g, ts_params.g, ts_params.g, pairing);
+    pairing_apply(ts_params.e_g_h, ts_params.g, ts_params.h, pairing);
+
+    element_init_G1(pkg_params.g, pairing); // 初始化G1群中PKG参数
+    element_init_G1(pkg_params.h, pairing);
+    element_init_G1(pkg_params.g1, pairing);
+    element_init_GT(pkg_params.e_g_g, pairing);
+    element_init_GT(pkg_params.e_g_h, pairing);
+    element_random(pkg_params.g);
+    element_random(pkg_params.h);
+    element_mul_zn(pkg_params.g1, pkg_params.g, pkg_priv);
+
+    pairing_apply(pkg_params.e_g_g, pkg_params.g, pkg_params.g, pairing);
+    pairing_apply(pkg_params.e_g_h, pkg_params.g, pkg_params.h, pairing);
+
+    // 打印生成元坐标
+    printf("生成TS参数:\n");
+    element_printf("ts_params.g = %B\n", ts_params.g); // 输出生成元的x,y坐标
+    element_printf("ts_params.h = %B\n", ts_params.h);
+    element_printf("ts_params.e_g_g = %B\n", ts_params.e_g_g);
+    element_printf("ts_params.e_g_h = %B\n", ts_params.e_g_h);
+
+    printf("生成PKG参数:\n");
+    element_printf("pkg_params.g = %B\n", pkg_params.g); // 输出生成元的x,y坐标
+    element_printf("pkg_params.h = %B\n", pkg_params.h);
+    element_printf("pkg_params.e_g_g = %B\n", pkg_params.e_g_g);
+    element_printf("pkg_params.e_g_h = %B\n", pkg_params.e_g_h);
+
+    // 清理内存
+    element_clear(ts_params.g);
+    element_clear(ts_params.g1);
+    element_clear(ts_params.h);
+    element_clear(ts_params.e_g_g);
+    element_clear(ts_params.e_g_h);
+    element_clear(pkg_params.g);
+    element_clear(pkg_params.h);
+    element_clear(pkg_params.g1);
+    element_clear(pkg_params.e_g_g);
+    element_clear(pkg_params.e_g_h);
+
+    // 清理配对参数
     pairing_clear(pairing);
 
     return 0;
